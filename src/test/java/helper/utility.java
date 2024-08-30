2c2912c1a3d6cff85563d7ac81418123bb6ba468
DaffaPintu <daffa@pintu.co.id>
2024-08-30 07:26:40 +0700

    FIX

--- src/test/java/helper/utility.java
+++ src/test/java/helper/utility.java
@@ -25,7 +25,7 @@ public class utility {
 
     public static void startDriver(){
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-allow-origins=*");
